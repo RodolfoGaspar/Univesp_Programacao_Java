ccfe96fc9349909467253d546d53de2cd11b03be
rodolfo <dolfogaspar@gmail.com>
2018-05-16 00:31:32 -0300

    Finalizado exercicio 2

--- Atividades/Semana2/Exerc_2.java
+++ Atividades/Semana2/Exerc_2.java
@@ -1,17 +1,24 @@
-class Exerc_1 {
+class Exerc_2 {
       public static void main(String[] args) {
+        System.out.println("Iniciando conversão de moeda");
+
         float vReal;
         float tCambioDolar;
         float vDolar;
 
-        vDolar = 5;
+        vDolar = 5.1F;
         tCambioDolar = 3.6F;
 
+        System.out.println("Dólar: US$"+vDolar);
+        System.out.println("Cotação: "+tCambioDolar);
+
         vReal = converteMoeda(vDolar, tCambioDolar);
+
+        System.out.println("Valor final: R$"+vReal);
        }
 
        public static float converteMoeda(float valor, float taxa)
        {
-         return valor * taxa
+         return valor * taxa;
        }
 }
