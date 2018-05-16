562cf0804f63bbad5a7ee4c05d92a1111a46cef1
rodolfo <dolfogaspar@gmail.com>
2018-05-16 00:22:06 -0300

    Add Ex 2 Semana 2

---
+++ Atividades/Semana2/Exerc_2.java
@@ -0,0 +1,12 @@
+class Exerc_1 {
+      public static void main(String[] args) {
+        float vReal;
+        float tCambioDolar;
+        float vDolar;
+
+        vDolar = 5;
+        tCambioDolar = 3.6F;
+
+        
+       }
+}
