dff5ea01a9ef91e9146a157665737d3abeea7477
rodolfo <dolfogaspar@gmail.com>
2018-05-16 00:10:41 -0300

    Add Ex 1 Semana 2

---
+++ Atividades/Semana2/Exerc_1.java
@@ -0,0 +1,14 @@
+class Exerc_1 {
+      public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = 8;
+        int d = a * (b + c * 3) - 7;
+        int e = a - b - c;
+        System.out.println(a+" "+b+" "+c+" "+d+" "+e);
+        a = a + 1;
+        b = (4 * a + 1) / 10;
+        c = (4 * a + 1) % 10;
+      System.out.println(a+" "+b+" "+c+" "+d+" "+e);
+       }
+}
