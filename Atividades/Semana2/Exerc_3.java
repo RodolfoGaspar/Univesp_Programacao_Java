8a26c6f83cbd6a42fd17c12999a41786aa2cc9ba
rodolfo <dolfogaspar@gmail.com>
2018-05-16 01:04:18 -0300

    Finalizado exercicio 3

--- Atividades/Semana2/Exerc_3.java
+++ Atividades/Semana2/Exerc_3.java
@@ -1,17 +1,22 @@
 class Exerc_3 {
       public static void main(String[] args) {
-        System.out.println("Iniciando cálculo de velocidade de queda...");
+        System.out.println("Iniciando cálculo de velocidade de queda...\n");
 
-        float altura = 100; //altura em centimetros
+        float altura = 1800; //altura em centimetros
+        float alturaEmMetros = altura/100; //altura em metros
         double tQueda; //tempo de queda em sengundos
         final float fGravidade = 9.8F;
 
-        tQueda = calculaTempoDeQueda(altura, fGravidade);
+        System.out.println("Altura: " + altura + "cm\n");
+        System.out.println("Altura: " + alturaEmMetros + "m\n");
+        System.out.println("Força da Gravidade: " + fGravidade + "m/s²\n");
 
-        System.out.println(tQueda);
+        tQueda = calculaTempoDeQueda(alturaEmMetros, fGravidade);
+
+        System.out.println("Tempo de queda: " + tQueda + "s");
       }
 
-      static double calculaTempoDeQueda(float altura, float fGravidade){
-        return Math.sqrt(2*altura/100/fGravidade);
+      static double calculaTempoDeQueda(float alturaEmMetros, float fGravidade){
+        return Math.sqrt(2*alturaEmMetros/fGravidade);
       }
 }
