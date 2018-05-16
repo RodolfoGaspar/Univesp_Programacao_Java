a0d0f5c0d90138cc044c6c2a085bd964532123cd
rodolfo <dolfogaspar@gmail.com>
2018-05-16 00:56:20 -0300

    Criado metodo de calculo de tempo de queda

--- Atividades/Semana2/Exerc_3.java
+++ Atividades/Semana2/Exerc_3.java
@@ -1,4 +1,17 @@
 class Exerc_3 {
       public static void main(String[] args) {
+        System.out.println("Iniciando cálculo de velocidade de queda...");
 
+        float altura = 100; //altura em centimetros
+        double tQueda; //tempo de queda em sengundos
+        final float fGravidade = 9.8F;
+
+        tQueda = calculaTempoDeQueda(altura, fGravidade);
+
+        System.out.println(tQueda);
+      }
+
+      static double calculaTempoDeQueda(float altura, float fGravidade){
+        return Math.sqrt(2*altura/100/fGravidade);
+      }
 }
