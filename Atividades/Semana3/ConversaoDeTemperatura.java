b2ecc1b182d153049d227b64a14db4e3936dfa96
rodolfo <dolfogaspar@gmail.com>
2018-05-23 13:51:45 -0300

    Adicionado classe de conversão de temperatura

---
+++ Atividades/Semana3/ConversaoDeTemperatura.java
@@ -0,0 +1,13 @@
+/*Atividade para avaliação da Semana 3*/
+
+class ConversaoDeTemperatura {
+      public static void main(String[] args) {
+        System.out.println("Iniciando conversão de temperatura...\n");
+
+
+
+        System.out.println("Tempo de queda: " + tQueda + "s");
+      }
+
+
+}
