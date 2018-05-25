9b7cfbb7671e07cd041d8cedb60e6f36d5014623
rodolfo <dolfogaspar@gmail.com>
2018-05-24 21:11:44 -0300

    Corrigido cálculo

--- Atividades/Semana3/NumeroPrimo.java
+++ Atividades/Semana3/NumeroPrimo.java
@@ -1,14 +1,22 @@
 /*
-Um primo é um número divisível apenas por 1 e por ele mesmo. A seguir tem-se instruções para a criação de um método que faz a verificação de números primos.
+Um primo é um número divisível apenas por 1 e por ele mesmo. A seguir
+tem-se instruções para a criação de um método que faz a verificação
+de números primos.
 
 O método deve receber um valor inteiro (int) positivo como parâmetro;
-Retornar true se o inteiro for um número primo e false se não. Você pode assumir o inteiro como sendo positivo, ou seja, não precisa testar essa condição.
-Usando o laço de repetição while, teste o resto da divisão desse número por 2, 3, . . ., até o inteiro anterior a ele. Se o resto de alguma dessas divisões for zero, então ele não é primo. Se após testar todos, nenhuma divisão resultou em resto zero, então ele só é divisível por 1 e por ele mesmo, ou seja, é primo.
+Retornar true se o inteiro for um número primo e false se não. Você pode
+assumir o inteiro como sendo positivo, ou seja, não precisa testar
+essa condição.
+Usando o laço de repetição while, teste o resto da divisão desse número
+por 2, 3, . . ., até o inteiro anterior a ele. Se o resto de alguma
+dessas divisões for zero, então ele não é primo. Se após testar todos,
+nenhuma divisão resultou em resto zero, então ele só é divisível
+por 1 e por ele mesmo, ou seja, é primo.
 */
 
 class NumeroPrimo {
       public static void main(String[] args) {
-        primo(20);
+        primo(35);
       }
 
       static boolean primo(int n) {
@@ -21,16 +29,17 @@ class NumeroPrimo {
           resto = n % div;
           if (resto == 0) {
             System.out.println(" -> Resto: "+ resto);
-            System.out.println("\n"+ n + " é primo.");
+            System.out.println("\n"+ n + " não é primo.");
 
-            return(true);
+            return(false);
           }
+
           System.out.println(" -> Resto: "+ resto);
           div++; //ou div = div+1 ou ++div
           System.out.print("Divindindo "+ n +" por " + div + ".");
         }
         System.out.println(" -> Resto: "+ resto);
-        System.out.println("\n" + n + " não é primo");
-        return(false);
+        System.out.println("\n" + n + " é primo");
+        return(true);
       }
 }
