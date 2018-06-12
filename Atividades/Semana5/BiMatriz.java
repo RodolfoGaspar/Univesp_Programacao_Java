93e55dcb619bc4bd5eb7c57665379f3ee4fb1db7
Rodolfo Gaspar <dolfogaspar@gmail.com>
2018-06-11 23:10:19 -0300

    Add file BiMatriz - Pergunta 2

---
+++ Atividades/Semana5/BiMatriz.java
@@ -0,0 +1,20 @@
+/*
+Observe as orientações abaixo com o objetivo de escrever um
+método que conta o número de elementos pares de uma matriz
+bidimensional:
+
+O método deve receber uma matriz bidimensional de inteiros
+como parâmetro;
+O método deve contar os números pares na matriz;
+Deve ser retornado o número de elementos pares existentes
+nessa matriz.
+De acordo com as informações acima, escolha qual das alternativas
+abaixo representa o código que retorna a quantidade de elementos
+pares de uma matriz bidimensional:
+*/
+
+class BiMatriz {
+    public static void main(String[] args){
+      
+    }
+  }
