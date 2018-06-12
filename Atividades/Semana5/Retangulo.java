f6f072c9d07971d6d879266b51615b2cc7f43609
Rodolfo Gaspar <dolfogaspar@gmail.com>
2018-06-12 00:06:24 -0300

    Add arquivo retangulo - pergunta 4

---
+++ Atividades/Semana5/Retangulo.java
@@ -0,0 +1,28 @@
+/*
+Visando a escrita de uma classe para representar retângulos
+utilizando a linguagem de programação Java (ou seja, cujos
+objetos representam retângulos específicos), as seguintes
+observações devem ser verificadas:
+
+1 - Essa classe deve conter 2 atributos de objeto (não de classe),
+correspondentes ao comprimento e altura do retângulo;
+2 - Estes atributos devem ser do tipo double;
+3 - A classe deve conter um construtor que receba, via seus
+parâmetros, a altura e comprimento do retângulo, abastecendo
+então os atributos do objeto;
+4 - Além disso, ela deve também conter os seguintes métodos:
+4.1 - Um método sem parâmetros que retorne a área do retângulo
+representado pelo objeto;
+4.2 - Um método sem parâmetros que retorne o valor do perímetro do
+retângulo representado pelo objeto.
+
+Sendo assim, analise quais das alternativas a seguir contém a
+classe em Java, descrita nas observações acima, utilizada para
+representar retângulos:
+*/
+
+class Retangulo {
+    public static void main(String[] args){
+      
+    }
+  }
